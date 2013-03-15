274bbfa5a384d214d4e3b1a432d3376f074ab427
Max Mays <cheeser12@ezen.com>
2013-03-15 19:49:14 -0400

    Added update method.

--- pysteroids.py
+++ pysteroids.py
@@ -1,26 +1,30 @@
 # Pysteroids - An Asteroids clone in Python
 # Max Mays
 import pyglet
-from lib.entities import Entity
-from lib.utils import Shape, Vector
-from math import asin, degrees
+from lib.entities import Ship
+from lib.utils import Vector
+from pyglet.window import key
 
 window = pyglet.window.Window(caption='Pysteroids')
 
-# Creates a test ship to demonstrate Entity and Shape classes
-# We set the rotation such that the ship will travel across
-# the diagnol of the window
-s = Shape((30, 0, -30, 20, -30, -20))
-e = Entity(s)
-windowSlope = Vector(window.width, window.height)
-windowSlope.normalize()
-e.rot = degrees(asin(windowSlope.y))
+# Get the keystate and create the ship
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
+ship = Ship(keys, pos=Vector(100, 100))
+
+
+# Update the components of the game and schedule
+# the function to run each frame
+def update(dt):
+    ship.update(dt)
+
+pyglet.clock.schedule(update)
 
 
 @window.event
 def on_draw():
     window.clear()
-    e.pos += e.direction * 2
-    e.draw()
+    ship.draw()
+
 
 pyglet.app.run()
