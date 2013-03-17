b7c97366f17b9a97a6d1c5765d59161ee6583bf9
Max Mays <cheeser12@ezen.com>
2013-03-16 21:54:43 -0400

    Added a shape to test collision

--- pysteroids.py
+++ pysteroids.py
@@ -2,7 +2,7 @@
 # Max Mays
 import pyglet
 from lib.entities import Ship
-from lib.utils import Vector
+from lib.utils import Vector, Shape
 from pyglet.window import key
 
 window = pyglet.window.Window(caption='Pysteroids')
@@ -12,11 +12,16 @@ keys = key.KeyStateHandler()
 window.push_handlers(keys)
 ship = Ship(keys, pos=Vector(100, 100))
 
+# Create a testShape to test collision
+testShape = Shape((100, 0, 0, -100, 0, 100),
+                  pos=Vector(300, 300), rot=0.0, scale=1.0)
+
 
 # Update the components of the game and schedule
 # the function to run each frame
 def update(dt):
     ship.update(dt)
+    print ship.collides(testShape)
 
 pyglet.clock.schedule(update)
 
@@ -25,6 +30,6 @@ pyglet.clock.schedule(update)
 def on_draw():
     window.clear()
     ship.draw()
-
+    testShape.draw()
 
 pyglet.app.run()
