347d3cda22cda0b02e8a3e9a8c26d3c6df31bd28
Max Mays <cheeser12@ezen.com>
2013-03-26 00:55:08 -0400

    Updated example

--- pysteroids.py
+++ pysteroids.py
@@ -1,8 +1,9 @@
+"""Entry point for running the game"""
 # Pysteroids - An Asteroids clone in Python
 # Max Mays
 import pyglet
-from lib.entities import Ship
-from lib.utils import Vector, Shape
+from lib.entities import Ship, Asteroid
+from lib.utils import Vector
 from pyglet.window import key
 
 window = pyglet.window.Window(caption='Pysteroids')
@@ -10,26 +11,36 @@ window = pyglet.window.Window(caption='Pysteroids')
 # Get the keystate and create the ship
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
-ship = Ship(keys, pos=Vector(100, 100))
+ship = Ship(pos=Vector(100, 100))
 
-# Create a testShape to test collision
-testShape = Shape((100, 0, 0, -100, 0, 100),
-                  pos=Vector(300, 300), rot=0.0, scale=1.0)
+# Create an asteroid
+asteroid = Asteroid(Asteroid.Size.SMALL, pos=Vector(300, 300))
+asteroid.direction = Vector(-1, -1)
+asteroid.lin_speed = 1
+asteroids = [asteroid]
 
 
 # Update the components of the game and schedule
 # the function to run each frame
 def update(dt):
-    ship.update(dt)
-    print ship.collides(testShape)
+    ship.update(keys, dt)
+    asteroid.update(dt)
+
+    # Detects a collision
+    if (ship.collides(asteroid)):
+        # Set the new asteroid array
+        global asteroids
+        asteroids = [ast for ast in asteroid.destroy()]
 
 pyglet.clock.schedule(update)
 
 
+# Draw the components to the screen
 @window.event
 def on_draw():
     window.clear()
     ship.draw()
-    testShape.draw()
+    for ast in asteroids:
+        ast.draw()
 
 pyglet.app.run()
