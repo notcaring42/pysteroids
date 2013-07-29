3ab3e58ba107c6a9fbf994ed34829f39db25e700
Max Mays <cheeser12@ezen.com>
2013-07-29 14:51:17 -0400

    Moved the Ship class to player.py since it is closely tied in with the Player class

--- lib/player.py
+++ lib/player.py
@@ -1,9 +1,15 @@
-"""Class which holds player variables and acts as a gateway to the
-Ship class"""
-from pyglet.clock import schedule_once
+"""Contains the Ship entity class which represents the player on the
+screen, and the Player class which holds player variables and wraps
+the Ship class
+"""
+from math import sin, cos, radians
+from random import randint
 
-from lib.utils import Vector, WINDOW_WIDTH, WINDOW_HEIGHT
-from lib.entities import Ship
+from pyglet.clock import schedule_once
+from pyglet.window import key
+
+from lib.utils import Vector, clamp, WINDOW_WIDTH, WINDOW_HEIGHT
+from lib.entities import Entity, Bullet
 
 
 class Player(object):
@@ -87,3 +93,158 @@ class Player(object):
             dt: not applicable, required for a clock callback function
         """
         self.is_vulnerable = True
+
+
+class Ship(Entity):
+    """Defines the player's ship.
+
+    An extension of the entity class, but with a self-defined shape and
+    input handling.
+
+    Attributes:
+        __max_speed: the maximum units per update that the ship can
+                     travel
+        __movement: the movement vector that the ship is translated by
+                    each frame
+        __shoot_delay: time that must elapse before the player can
+                       shoot again
+        __teleport_delay: time that must elapse before the player can
+                          teleport again
+        __last_shoot: amount of time since the player last shot
+        __last_teleport: amount of time since the player last
+                         teleported
+        bullets: a list of bullets shot by the player that are in play
+    """
+
+    __max_speed = 1.5
+    __shoot_delay = 0.8
+    __teleport_delay = 7.0
+
+    def __init__(self, pos=Vector(0, 0), rot=0.0):
+        """Creates a new Ship
+
+        Args:
+            pos: the position of the ship
+            rot: the rotation of the ship
+
+        Returns:
+            a new Ship instance
+        """
+
+        # Get the initial direction of the ship using the rotation
+        direction = Vector(cos(radians(rot)), sin(radians(rot)))
+
+        # Initially, the ship isn't moving
+        self.__movement = Vector.zero()
+
+        # Initialize bullet list
+        self.bullets = []
+        self.__last_shoot = self.__shoot_delay
+        self.__last_teleport = self.__teleport_delay
+
+        Entity.__init__(self, (20, 0, -30, 20, -30, -20), direction,
+                        lin_speed=1.0, rot_speed=1.8, pos=pos, rot=rot,
+                        scale=0.5)
+
+    def __handle_input(self, keys, dt):
+        """Handles any input from the player.
+
+        Args:
+            keys: the keyboard state
+            dt: the amount of time since the last update
+        """
+
+        # Moves the ship forward
+        #
+        # Moving works like this: each frame the ship is translated
+        # by the movement vector. When the ship moves forward, a scalar
+        # multiple of the direction vector is added to the movement
+        # vector. This simulates zero-gravity movement for the ship:
+        # even if the player is facing a different direction, the
+        # movement still continues in the same direction until
+        # thrusters are engaged
+        if keys[key.W]:
+            self.__movement += self._direction * self.lin_speed * dt
+
+        # Set a maximum speed
+        # Normalize the movement vector and then multiply
+        # by the new length to get the new speed
+        new_length = clamp(self.__movement.length(),
+                           -self.__max_speed, self.__max_speed)
+        self.__movement = self.__movement.normalize()
+        self.__movement *= new_length
+
+        # Rotate the ship
+        if keys[key.A]:
+            self.rot += self.rot_speed
+        elif keys[key.D]:
+            self.rot -= self.rot_speed
+
+        # Shoot a bullet, but check to make sure that the time
+        # since the last shot is greater than the delay
+        if keys[key.SPACE] and (self.__last_shoot >= self.__shoot_delay):
+            bullet_pos = self.pos + 3*self.direction
+            self.bullets.append(Bullet(bullet_pos, self.rot,
+                                       self.direction))
+            self.__last_shoot = 0
+        else:
+            self.__last_shoot += dt
+
+        # Teleport the ship to a random location
+        if keys[key.LSHIFT] and (self.__last_teleport >=
+                                 self.__teleport_delay):
+            # We give a buffer of 20 units so the ship doesn't teleport
+            # to the very edge of the screen
+            self.pos = Vector(randint(20, WINDOW_WIDTH-20),
+                              randint(20, WINDOW_HEIGHT-20))
+            self.__last_teleport = 0
+        else:
+            self.__last_teleport += dt
+
+    def update(self, keys, dt):
+        """Updates the ship and handles input
+
+        Args:
+            keys: the keyboard state
+            dt: the amount of time since the last update
+        """
+        # Move the ship
+        self.__handle_input(keys, dt)
+        self.pos += self.__movement
+
+        # Update variables
+        self._shape.pos = self.pos
+        self._shape.rot = self.rot
+        self._shape.scale = self.scale
+
+        # Reflect across the screen, if necessary
+        self._reflect_across_screen()
+
+        # Update each bullet, and remove if necessary
+        for bullet in self.bullets:
+            bullet.update(dt)
+
+            if bullet.expired is True:
+                self.bullets.remove(bullet)
+
+    def draw(self):
+        Entity.draw(self)
+        for bullet in self.bullets:
+            bullet.draw()
+
+    # Direction stored as a property to ensure normalization
+    @property
+    def direction(self):
+        return self._direction
+
+    # Setting the rotation of the ship also sets the new direction
+    @property
+    def rot(self):
+        return self.__rot
+
+    @rot.setter
+    def rot(self, value):
+        self.__rot = value
+
+        # Set new direction vector
+        self._direction = Vector(cos(radians(value)), sin(radians(value)))
