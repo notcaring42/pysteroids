76a0803a461e135f746bb5a95cb989274df179e6
Max Mays <cheeser12@ezen.com>
2013-08-25 22:18:44 -0400

    Added long description using README.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.txt') as file:
+    long_description = file.read()
+
 setup(
     name = 'pysteroids',
     version = '1.0',
     description = ('A clone of the game Asteroids, '
                    'with some extra bells and whistles'),
+    long_description = long_description,
     author = 'Max Mays',
     author_email = 'wmays@max-mays.com',
     keywords = 'game asteroids',
